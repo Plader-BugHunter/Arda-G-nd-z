249ffbc86c2a8026f4bc2e193bf53736f89d21c8
Arda Gunduz <84984503+Plader-BugHunter@users.noreply.github.com>
2021-10-02 10:30:34 +0300

    Update Price Tracker.py

--- Price Tracker.py
+++ Price Tracker.py
@@ -32,7 +32,7 @@ url1 ="https://www.trendyol.com/sony/playstation-5-825-gb-dijital-surum-turkce-m
 
 
 headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+    "User-Agent":"You Go Write What İs My User Agent"}
 
 page=requests.get(url1, headers=headers)
 
